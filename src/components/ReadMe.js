c4b911d4933b06bde1ee3a824ad9709f467e57c2
Ayech0x2 <4yech.hamza@gmail.com>
2018-08-23 12:19:20 +0100

    -Read me adding missing text.

--- src/components/ReadMe.js
+++ src/components/ReadMe.js
@@ -1,73 +1,83 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 export default class ReadMe extends React.Component {
   render() {
     return (
-      <div className="container mt-10 mb-5 read-me flex-wrap justify-content-center">
-        <div className="m-2 d-flex flex-column align-items-center">
-          <div className="box ">
-            <div className="little-box">
-              <span>10</span>
-              <span> june</span>
+      <Fragment>
+        <div className="container text-center mt-10 d-flex flex-column">
+          <span className="text-uppercase read-me-txt">
+            read me
+          </span>
+            <span className="read-me-txt-2">
+            sometimes i write something smart
+          </span>
+        </div>
+        <div className="container mt-5 mb-5 read-me flex-wrap justify-content-center">
+          <div className="m-2 d-flex flex-column align-items-center">
+            <div className="box ">
+              <div className="little-box">
+                <span>10</span>
+                <span> june</span>
+              </div>
+            </div>
+            <div className="pt-2 pb-2 d-flex flex-column like-box justify-content-center">
+              <span className="card-title ">TEN THE BEST APP IN 2017</span>
+              <span className="card-category ">Category</span>
+              <span className="card-category ">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas pellentesque eu enim eget luctus. Sed augue felis,
+                facilisis et elementum vitae, aliquam sit amet ante. Sed iaculis
+                eros sem, elementum consequat.
+              </span>
+            </div>
+            <div className=" m-4 justify-content-start">
+              <span className="btn-none-rounded ">read more</span>
             </div>
           </div>
-          <div className="pt-2 pb-2 d-flex flex-column like-box justify-content-center">
-            <span className="card-title ">TEN THE BEST APP IN 2017</span>
-            <span className="card-category ">Category</span>
-            <span className="card-category ">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              pellentesque eu enim eget luctus. Sed augue felis, facilisis et
-              elementum vitae, aliquam sit amet ante. Sed iaculis eros sem,
-              elementum consequat.
-            </span>
-          </div>
-          <div className=" m-4 justify-content-start">
-            <span className="btn-none-rounded ">read more</span>
-          </div>
-        </div>
-        <div className="m-2 d-flex flex-column align-items-center">
-          <div className="box ">
-            <div className="little-box">
-              <span>10</span>
-              <span> june</span>
+          <div className="m-2 d-flex flex-column align-items-center">
+            <div className="box ">
+              <div className="little-box">
+                <span>10</span>
+                <span> june</span>
+              </div>
+            </div>
+            <div className="pt-2 pb-2 d-flex flex-column like-box justify-content-center">
+              <span className="card-title ">TEN THE BEST APP IN 2017</span>
+              <span className="card-category ">Category</span>
+              <span className="card-category ">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas pellentesque eu enim eget luctus. Sed augue felis,
+                facilisis et elementum vitae, aliquam sit amet ante. Sed iaculis
+                eros sem, elementum consequat.
+              </span>
+            </div>
+            <div className=" m-4 justify-content-start">
+              <span className="btn-none-rounded ">read more</span>
             </div>
           </div>
-          <div className="pt-2 pb-2 d-flex flex-column like-box justify-content-center">
-            <span className="card-title ">TEN THE BEST APP IN 2017</span>
-            <span className="card-category ">Category</span>
-            <span className="card-category ">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              pellentesque eu enim eget luctus. Sed augue felis, facilisis et
-              elementum vitae, aliquam sit amet ante. Sed iaculis eros sem,
-              elementum consequat.
-            </span>
-          </div>
-          <div className=" m-4 justify-content-start">
-            <span className="btn-none-rounded ">read more</span>
-          </div>
-        </div>
-        <div className="m-2 d-flex flex-column align-items-center">
-          <div className="box ">
-            <div className="little-box">
-              <span>10</span>
-              <span> june</span>
+          <div className="m-2 d-flex flex-column align-items-center">
+            <div className="box ">
+              <div className="little-box">
+                <span>10</span>
+                <span> june</span>
+              </div>
+            </div>
+            <div className="pt-2 pb-2 d-flex flex-column like-box justify-content-center">
+              <span className="card-title ">TEN THE BEST APP IN 2017</span>
+              <span className="card-category ">Category</span>
+              <span className="card-category ">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas pellentesque eu enim eget luctus. Sed augue felis,
+                facilisis et elementum vitae, aliquam sit amet ante. Sed iaculis
+                eros sem, elementum consequat.
+              </span>
+            </div>
+            <div className=" m-4 justify-content-start">
+              <span className="btn-none-rounded ">read more</span>
             </div>
           </div>
-          <div className="pt-2 pb-2 d-flex flex-column like-box justify-content-center">
-            <span className="card-title ">TEN THE BEST APP IN 2017</span>
-            <span className="card-category ">Category</span>
-            <span className="card-category ">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              pellentesque eu enim eget luctus. Sed augue felis, facilisis et
-              elementum vitae, aliquam sit amet ante. Sed iaculis eros sem,
-              elementum consequat.
-            </span>
-          </div>
-          <div className=" m-4 justify-content-start">
-            <span className="btn-none-rounded ">read more</span>
-          </div>
         </div>
-      </div>
+      </Fragment>
     );
   }
 }
