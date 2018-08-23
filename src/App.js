4c09840c0c3fb0b9813c9286401cbd4486255041
Ayech0x2 <4yech.hamza@gmail.com>
2018-08-23 10:29:41 +0100

    -Removing Header component -Adding react-scroll -Navigation click events.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import React, { Component } from "react";
 import "./App.css";
-import Header from "./components/Header";
 import Top from "./components/Top";
 import Work from "./components/Work";
 import AboutMe from "./components/AboutMe";
 import Testimonial from "./components/Testimonial";
 import ReadMe from "./components/ReadMe";
 import Footer from "./components/Footer";
+import {
+  Element,
+  Events,
+  animateScroll as scroll,
+  scroller
+} from "react-scroll";
+import Navigation from "./components/Navigation";
 
 class App extends Component {
+  componentDidMount() {
+    Events.scrollEvent.register("begin", function() {
+      console.log("begin", arguments);
+    });
+
+    Events.scrollEvent.register("end", function() {
+      console.log("end", arguments);
+    });
+  }
+
+  scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+  scrollTo = name => {
+    scroller.scrollTo(name, {
+      duration: 800,
+      delay: 0,
+      smooth: "easeInOutQuart"
+    });
+  };
   render() {
     return (
       <div className="container-fluid remove-padding">
-        <Header />
-        <Top />
-        <Work />
-        <AboutMe />
-        <Testimonial />
-        <ReadMe />
-        <Footer />
+        <Navigation scrollTo={this.scrollTo} />
+        <Element>
+          <Top />
+        </Element>
+        <Element name="work">
+          <Work />
+        </Element>
+        <Element name="about">
+          <AboutMe />
+        </Element>
+        <Element>
+          <Testimonial />
+        </Element>
+        <Element name="blog">
+          <ReadMe />
+        </Element>
+        <Element>
+          <Footer />
+        </Element>
       </div>
     );
   }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -32,6 +32,9 @@ export default class Navigation extends React.Component {
       modal: !this.state.modal
     });
   };
+  handleMenu = name => {
+    this.props.scrollTo(name);
+  };
   render() {
     return (
       <div className="container navigation-bar remove-padding">
@@ -53,7 +56,52 @@ export default class Navigation extends React.Component {
           </NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
-            <NavigationItems />
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink
+                  className="menu-item"
+                  onClick={() => this.handleMenu("work")}
+                >
+                  work
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className="menu-item"
+                  href="#"
+                  onClick={() => this.handleMenu("about")}
+                >
+                  about me
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  onClick={() => this.handleMenu("blog")}
+                  className="menu-item"
+                  href="#"
+                >
+                  blog
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className="menu-item"
+                  href="#"
+                  onClick={() => this.handleMenu("contact")}
+                >
+                  contact
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className="menu-item rounded-link"
+                  href="{"
+                  id="nav-lastchild"
+                >
+                  hire me
+                </NavLink>
+              </NavItem>
+            </Nav>
           </Collapse>
         </Navbar>
         <Modal isOpen={this.state.modal} toggle={this.toggleModal}>
@@ -71,48 +119,3 @@ export default class Navigation extends React.Component {
     );
   }
 }
-
-export const NavigationItems = () => {
-  return (
-    <Nav className="ml-auto" navbar>
-      <NavItem>
-        <NavLink className="menu-item" href="/components/ ">
-          work
-        </NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink
-          className="menu-item"
-          href="https://github.com/reactstrap/reactstrap"
-        >
-          about me
-        </NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink
-          className="menu-item"
-          href="https://github.com/reactstrap/reactstrap"
-        >
-          blog
-        </NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink
-          className="menu-item"
-          href="https://github.com/reactstrap/reactstrap"
-        >
-          contact
-        </NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink
-          className="menu-item rounded-link"
-          href="https://github.com/reactstrap/reactstrap"
-          id="nav-lastchild"
-        >
-          hire me
-        </NavLink>
-      </NavItem>
-    </Nav>
-  );
-};
